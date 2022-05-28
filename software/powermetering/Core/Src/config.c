ad3282528c57af41465d9b13dd9243e8eb8d8a0d
Maurice Massar <massar@unix-ag.uni-kl.de>
2022-05-28 20:53:11 +0200

    fix config parsing

--- software/powermetering/Core/Src/config.c
+++ software/powermetering/Core/Src/config.c
@@ -63,39 +63,35 @@ uint32_t config_read(uint8_t* data, uint32_t size){
 void config_get_string(const char* key2, char* value2){
     // SEGGER_RTT_printf(0, "getting string %s\n",key2);
     // SEGGER_RTT_printf(0, "string: %s\n",flash_config);
+    char keyvalue[CONFIG_STRINGLENGTH*2+2];
     char key[CONFIG_STRINGLENGTH];
     char value[CONFIG_STRINGLENGTH];
     int ret = 0;
     int pos = 0;
     int keys_left = 1;
 
-    do{
-        ret = sscanf(flash_config+pos,"%32[a-zA-Z_]=%32[a-zA-Z0-9._-\%]",key,value);
-        if(ret == 2){
-            pos += strlen(key) + strlen(value) + 1;
-            //printf("next char: %c\n",*(string + pos));
-            if(*(flash_config + pos) == '&' || *(flash_config + pos) == '\0'){
-                if(*(flash_config + pos) == '\0'){//next char is NULL, end of string
-                    //printf("last value!\n");
-                    keys_left = 0;
-                }else{
-                    pos++;
-                }
-            }else{//No keys left
-                keys_left = 0;
-            }
-            // SEGGER_RTT_printf(0, "%d %s => %s\n",ret,key,value);
-        }else{//sscanf did not find a key value pair
-            // SEGGER_RTT_printf(0, "scanf return %d\n",ret);
-            keys_left = 0;
+    while (flash_config[pos] != '\0') {
+        ret = sscanf(flash_config+pos, "%65[^&]", keyvalue);
+        if (ret != 1) {
+            break;
         }
+        
+        // move pointer to next string, skip '&' if exists (not \0)
+        pos += strlen(keyvalue);
+        if (flash_config[pos] == '&')
+            pos++;
+        
+        ret = sscanf(keyvalue,"%32[^=]=%32[^=]",key,value);
+        if(ret != 2)
+            continue;
+
         if(strncmp(key2,key,CONFIG_STRINGLENGTH) == 0){
-            // SEGGER_RTT_printf(0, "found %s\n",value);
+            SEGGER_RTT_printf(0, "found %s => %s\n", key, value);
             strncpy(value2,value,CONFIG_STRINGLENGTH);
             break;
         }
         //printf("next parsed char: '%c'\n",*(string+pos));
-    }while(keys_left);
+    }
 }
 
 void config_get_int(const char* name, int32_t* value){
